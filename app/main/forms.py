3da6cb30f6db90512a622de4585956b409ffb075
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-11-28 14:16:18 +0300

    add main forms

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import Required
+
+class SubmitPhoto(FlaskForm):
+
+    description = TextArea('Text', Validators=[Required()])
+    category  = SelectedField('Type', choices=[('places', 'Place Photos'), ('people', 'People Photos'), ('work', 'Work Photos'), ('food', 'Food Photos'), ('activate', 'Active Photos')], validators=[Required()])
+    submit = SubmitField('Submit')
